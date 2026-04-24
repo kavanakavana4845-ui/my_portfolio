2b3167d0d2658986e68df5e1de594c49b3c775be
kavanakavana484-ui <kavanakavana484@gmail.com>
2026-04-24 11:29:11 +0530

    Day 3: dark light mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("kavana it is working!")
+// Step A: grab the button
+const themeBtn = document.querySelector('#theme-btn')
+
+// Step B: listen for click
+themeBtn.addEventListener('click', function() {
+
+  // Step C: toggle light class on body
+  document.body.classList.toggle('light')
+
+  // Step D: change the emoji
+  if (document.body.classList.contains('light')) {
+    themeBtn.textContent = '☀️'
+  } else {
+    themeBtn.textContent = '🌙'
+  }
+
+})
